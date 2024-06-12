bd83f1c6a7f458fce014d1c2fb28180ec08cfe1d
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-06-12 20:58:45 -0300

    computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,18 +11,18 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={1.5} groundColor='black' />
       <spotLight
-        position={[120, 50, 10]}
+        position={[isMobile ? 50 : 120, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={isMobile ? 0.5 : 1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={isMobile ? 0.5 : 1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.35 : 0.75}
-        position={isMobile ? [-10, -2, -3.0] : [1, -3, -2.5]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -2, 0] : [1, -3, -2.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -33,21 +33,15 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -58,7 +52,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: isMobile ? [10, 3, 5] : [20, 3, 5], fov: isMobile ? 35 : 25 }}
+      camera={{ position: isMobile ? [10, 5, 10] : [20, 3, 5], fov: isMobile ? 45 : 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
